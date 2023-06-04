ef1aa6eba29163a237dfa84560626bf6ae6217dc
imshivang03 <shivang.cse.nitnagaland@gmail.com>
2023-06-04 16:31:33 +0530

    Prediction Pipeline Completed

--- src/utils.py
+++ src/utils.py
@@ -18,6 +18,13 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException (e, sys)
     
+def load_object(file_path):
+    try:
+        with open (file_path, "rb") as file_obj:
+            return pickle.dump(file_obj)
+
+    except Exception as e:
+        raise CustomException (e, sys)
 
 def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
